e7e08a5f768c68cce4e220887b18020f9e18cbb0
Cassidy Williams <cassidoo@gmail.com>
2019-11-23 01:21:55 -0500

    Add commenting around how star ratings are broken up

--- courses/fundamentals/app/ui/ProductRating.js
+++ courses/fundamentals/app/ui/ProductRating.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import { FaStarHalf, FaStar } from 'react-icons/fa'
+import { FaRegStar, FaStarHalfAlt, FaStar } from 'react-icons/fa'
 import './ProductRating.scss'
 
 function ProductRating({ rating }) {
   let stars = []
 
+  // Add filled in star(s)
   for (let i = 0; i < Math.floor(rating); i++) {
     stars.push(<FaStar key={`star-${i}`} />)
   }
 
-  if (rating % 1 > 0) stars.push(<FaStarHalf key={`star-half`} />)
+  // Add half star
+  if (rating % 1 > 0) stars.push(<FaStarHalfAlt key={`star-half`} />)
+
+  // Add empty star(s)
+  while (stars.length < 5) stars.push(<FaRegStar key={`star-empty-${stars.length}`} />)
 
   return <span>{stars}</span>
 }
