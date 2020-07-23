ae935521218720a1e8cfcc12d0416984f25b78a6
Brad Westfall <brad@azpixels.com>
2020-07-22 17:25:43 -0700

    small refactor on compound components in electives

--- courses/electives/compound-components/lecture/index.js
+++ courses/electives/compound-components/lecture/index.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import ReactDOM from 'react-dom'
 import LoginForm from 'YesterTech/LoginForm'
 import SignupForm from 'YesterTech/SignupForm'
-import { Tabs } from './Tabs'
 import 'YesterTech/styles/global-styles.scss'
 import './styles.scss'
+
+export function Tabs({ data, ...props }) {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  return (
+    <div data-reach-tabs>
+      <div data-reach-tab-list>
+        {data.map((tab, index) => {
+          const selected = index === selectedIndex
+          return (
+            <button
+              key={index}
+              role="tab"
+              {...props}
+              aria-selected={selected}
+              data-reach-tab=""
+              data-selected={selected ? '' : undefined}
+              onClick={() => setSelectedIndex(index)}
+            >
+              {tab.label}
+            </button>
+          )
+        })}
+      </div>
+      <div data-reach-tab-panels="">
+        <div data-reach-tab-panel="">
+          {data[selectedIndex].content}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   const tabData = [
