06c07c9bf3d81ab3abcb04e2a82731cc24553af2
Brad Westfall <bradwestfall@brads-mbp.lan>
2020-09-21 09:05:47 -0700

    import non-final

--- courses/advanced-hooks/02-imperative-react/lecture/index.js
+++ courses/advanced-hooks/02-imperative-react/lecture/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import App from './app'
-import App from './app.final'
+import App from './app'
+// import App from './app.final'
 import './styles.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
