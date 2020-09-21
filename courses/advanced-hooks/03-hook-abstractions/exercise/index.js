1001981ad4337c975d2c26c9158afdcab3136bcc
Brad Westfall <bradwestfall@brads-mbp.lan>
2020-09-21 15:38:39 -0700

    remove empty exercise

--- courses/advanced-hooks/03-hook-abstractions/exercise/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useMemo } from 'react'
-
-import ReactDOM from 'react-dom'
-
-function App() {
-  return <div></div>
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
