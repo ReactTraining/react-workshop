8c82542d371e5e69ab3553326a617b56ac213574
Brad Westfall <brad@azpixels.com>
2020-10-05 08:48:52 -0700

    starting point for 1

--- courses/core/01-rendering/lecture/index.js
+++ courses/core/01-rendering/lecture/index.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import { FaRegStar, FaStar } from 'react-icons/fa'
 // import { MdShoppingCart } from 'react-icons/md'
 import 'YesterTech/styles/global-styles.scss'
 import './styles.scss'
 
-function test() {
-  const x = 8
-  if (true) {
-    const x = 7
-  }
-}
+// Let's go
