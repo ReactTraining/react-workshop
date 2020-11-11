7b0e52e98a754ad30ffec13cdef894254b5bc5ac
Brad Westfall <brad@azpixels.com>
2020-11-11 10:45:14 -0700

    success notice

--- apps/YesterTech/Notice.js
+++ apps/YesterTech/Notice.js
@@ -4,12 +4,12 @@ import classnames from 'classnames'
 
 import 'YesterTech/Notice.scss'
 
-function Notice({ children, type = 'error' }) {
+function Notice({ children, type = 'default' }) {
   return <div className={classnames('notice', `notice-type-${type}`)}>{children}</div>
 }
 
 Notice.propTypes = {
-  type: PropTypes.oneOf(['error', 'success'])
+  type: PropTypes.oneOf(['default', 'error', 'success'])
 }
 
 export default Notice
