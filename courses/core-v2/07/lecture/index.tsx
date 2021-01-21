528b6ad2d7c70be9196e6773362a9ac2220d9c40
Brad Westfall <brad@azpixels.com>
2021-01-21 13:19:57 -0700

    sass variables to css custom properties

---
+++ courses/core-v2/07/lecture/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'ProjectPlanner/styles/global-styles.scss'
+import './styles.scss'
+
+function App() {
+  return <div></div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
