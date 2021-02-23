3f59ca56db0f44490d13829a50f5568897f7fc5e
Brad Westfall <brad@azpixels.com>
2021-02-23 09:16:28 -0700

    starting point for advanced hooks changed

--- courses/advanced-hooks/01-effects/lecture/index.tsx
+++ courses/advanced-hooks/01-effects/lecture/index.tsx
@@ -6,8 +6,8 @@ import * as ReactDOM from 'react-dom'
  */
 
 // 1. The closure of useEffect "captures" state (basic example)
-// import App from './closure-basics'
-// ReactDOM.render(<App />, document.getElementById('root'))
+import App from './closure-basics'
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // 2. Another capturing example (Stopwatch)
 // import App from './stopwatch'
