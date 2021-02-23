7710d2e03053ac25f2b47e39bf7713b033b5b21f
Brad Westfall <brad@azpixels.com>
2021-02-23 10:18:05 -0700

    chance starting point for lecture

--- courses/advanced-hooks/02-imperative-react/lecture/app.tsx
+++ courses/advanced-hooks/02-imperative-react/lecture/app.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import React, { useEffect, useState, useRef } from 'react'
+import ReactDOM from 'react-dom'
 import { position } from './utils'
 import './styles.scss'
 
