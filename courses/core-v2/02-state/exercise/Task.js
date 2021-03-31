a5fef46eca60de58aeb8caa2d036723a876631d3
Brad Westfall <brad@azpixels.com>
2021-03-31 11:33:10 -0700

    fixing readme for exercise

--- courses/core-v2/02-state/exercise/Task.js
+++ courses/core-v2/02-state/exercise/Task.js
@@ -15,8 +15,7 @@ export const Task = () => {
   // Task 2: You need to "lift" the state from the Minutes component
   // to here. That means you'll move the useState() line in Minutes
   // to here and pass down a `minutes` and an `onChange` prop like this:
-  // <Minutes minutes={minutes} onChange={fn}>
-  // Replace fn with a function that calls setMinutes.
+  // <Minutes minutes={minutes} setMinutes={setMinutes}>
   const minutes = 8
 
   // For more tasks, see the README.md
