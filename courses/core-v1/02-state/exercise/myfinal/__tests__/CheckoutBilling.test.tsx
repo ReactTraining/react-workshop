82488b558374cce34b85d60ccf59876537ca1dd3
b2 <brendan@mobilewebsmart.com>
2021-04-16 09:45:31 -0600

    core-v1/02-state

--- courses/core-v1/02-state/exercise/myfinal/__tests__/CheckoutBilling.test.tsx
+++ courses/core-v1/02-state/exercise/myfinal/__tests__/CheckoutBilling.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import CheckoutBilling from '../CheckoutBilling'
 
 const mockOnSubmit = jest.fn((...args:any) => {})
@@ -16,12 +17,12 @@ describe('<CheckoutBilling />', () => {
     render(<CheckoutBilling onSubmit={mockOnSubmit} />)
 
     const sameAsBillingCheckbox = await screen.findByLabelText(/same as billing/i)
-    await fireEvent.click(sameAsBillingCheckbox)
+    fireEvent.click(sameAsBillingCheckbox)
 
     expect((await screen.findAllByLabelText(/name/i)).length).toBe(1)
     expect((await screen.findAllByLabelText(/address/i)).length).toBe(1)
 
-    await fireEvent.click(sameAsBillingCheckbox)
+    fireEvent.click(sameAsBillingCheckbox)
 
     expect((await screen.findAllByLabelText(/name/i)).length).toBe(2)
     expect((await screen.findAllByLabelText(/address/i)).length).toBe(2)
@@ -45,10 +46,12 @@ describe('<CheckoutBilling />', () => {
 		fireEvent.input(input, { target: { value: "John Doe" } } );
 	});
 	(await screen.findAllByLabelText(/address/i)).forEach((input) => {
-		fireEvent.input(input, { target: { value: "123 Fake St." } } );
+		// alternative to using fireEvent.input:
+		userEvent.type(input, "123 Fake St.");
 	});
 	
-	fireEvent.click(submitButton);
+	// alternative to using fireEvent.click(submitButton):
+	userEvent.keyboard('{enter}');
 
 	expect(mockOnSubmit).toHaveBeenCalledWith({
 		"billingName": "John Doe",
