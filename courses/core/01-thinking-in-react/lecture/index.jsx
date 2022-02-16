cbb09aaf1f69953d21d78f8bb6352e7e44c9a8c1
Brad Westfall <brad@azpixels.com>
2022-02-16 12:58:23 -0700

    new starging point for lesson 1

--- courses/core/01-thinking-in-react/lecture/index.jsx
+++ courses/core/01-thinking-in-react/lecture/index.jsx
@@ -1,5 +1,7 @@
-// import React from 'react' <-- We don't need this anymore
+// import React from 'react' <-- We don't need this anymore (React 17)
 import ReactDOM from 'react-dom'
+import { FaTrash } from 'react-icons/fa'
+import './styles.scss'
 
 // This is our "entry" file for our build system.
 // Let's start making our React app and "mount" it
