9277a221ed773f04c38656677bfdbd97de02e578
Brad Westfall <brad@azpixels.com>
2022-02-16 12:51:37 -0700

    new starting for lesson 1

--- courses/core/01-thinking-in-react/lecture/index.jsx
+++ courses/core/01-thinking-in-react/lecture/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+// import React from 'react' <-- We don't need this anymore
 import ReactDOM from 'react-dom'
 
-// Let's go
+// This is our "entry" file for our build system.
+// Let's start making our React app and "mount" it
+// to the DOM
