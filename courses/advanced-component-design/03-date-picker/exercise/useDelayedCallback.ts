bb19a4af07a18c4aa42a9b81c3c62e84f37448ba
Brad Westfall <brad@azpixels.com>
2022-06-20 10:57:09 -0400

    fix

--- courses/advanced-component-design/03-date-picker/exercise/useDelayedCallback.ts
+++ courses/advanced-component-design/03-date-picker/exercise/useDelayedCallback.ts
@@ -11,7 +11,10 @@ export function useDelayedCallback<T>(cb: (v: T) => void) {
 
   useEffect(() => {
     if (callbackValue !== null) {
-      const id = setTimeout(() => cb(callbackValue), ms)
+      const id = setTimeout(() => {
+        cb(callbackValue)
+        setCallbackValue(null)
+      }, ms)
       return () => clearTimeout(id)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
