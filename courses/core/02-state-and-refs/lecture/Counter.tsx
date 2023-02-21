df88f3051c8b835b3b6755e15ca3cf66c43760c6
Brad Westfall <brad@azpixels.com>
2023-02-21 12:34:24 -0500

    progress on workshop

--- courses/core/02-state-and-refs/lecture/Counter.tsx
+++ courses/core/02-state-and-refs/lecture/Counter.tsx
@@ -1,7 +1,7 @@
 // Read more about imports with React
 // https://twitter.com/dan_abramov/status/1308739731551858689
 
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 import { Icon } from 'course-platform/Icon'
 
 // type CounterProps = {
@@ -10,19 +10,29 @@ import { Icon } from 'course-platform/Icon'
 //   min?: number
 // }
 
-export function Counter() {
-  let count = 0
+export function Counter({ min = 0 }) {
+  const [count, setCount] = useState(0)
+
+  function subtract() {
+    if (count > min) {
+      setCount(count - 1)
+    }
+  }
+
+  function add() {
+    setCount(count + 1)
+  }
 
   return (
     <div className="counter inline-flex flex-gap">
       <div>
-        <button className="button button-small">
+        <button onClick={subtract} className="button button-small">
           <Icon name="minus" />
         </button>
       </div>
       <div className="input">{count}</div>
       <div>
-        <button className="button button-small">
+        <button onClick={add} className="button button-small">
           <Icon name="plus" />
         </button>
       </div>
