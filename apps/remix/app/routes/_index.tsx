54b77985af04d653fd49845b602125fd040543e5
Brad Westfall <brad@builtwithweb.com>
2023-04-11 23:22:05 -0400

    playing with remix

--- apps/remix/app/routes/_index.tsx
+++
@@ -1,9 +0,0 @@
-// import type { V2_MetaFunction } from '@remix-run/node'
-
-// export const meta: V2_MetaFunction = () => {
-//   return [{ title: 'New Remix App' }]
-// }
-
-export default function Index() {
-  return <div>Base App</div>
-}
--- scripts/start-remix.js
+++
@@ -1,4 +0,0 @@
-const path = require('path')
-
-const appPath = path.resolve(__dirname, 'apps/remix')
-const lecturePath = path.resolve(__dirname, 'courses/remix/01-intro')
