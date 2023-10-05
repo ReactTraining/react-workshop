f8e36438d99290d0f02dc54a103b8a9976f6362e
Brad Westfall <brad@builtwithweb.com>
2023-10-05 12:06:08 -0400

    end of day 3

--- react/advanced-hooks/02-useLayoutEffect/lecture/AccountSidebar.tsx
+++ react/advanced-hooks/02-useLayoutEffect/lecture/AccountSidebar.tsx
@@ -2,8 +2,26 @@ import { useState, useEffect, useLayoutEffect } from 'react'
 import { AccountFavorites } from '~/AccountFavorites'
 import { Heading } from '~/Heading'
 
-export function AccountSidebar() {
-  const [isWide, setIsWide] = useState(true)
+function useMedia(query: string) {
+  const [matches, setMatches] = useState(false)
+
+  useLayoutEffect(() => {
+    const media = window.matchMedia(query)
+    setMatches(media.matches)
+    const listener = () => {
+      setMatches(media.matches)
+    }
+    media.addEventListener('change', listener)
+    return () => {
+      media.removeEventListener('change', listener)
+    }
+  }, [query])
+
+  return matches
+}
+
+export function AccountSidebar({ width = 1200 }) {
+  const isWide = useMedia(`(min-width: ${width}px)`)
 
   return isWide ? (
     <aside className="w-60 pr-6 border-r border-slate-300 space-y-6">
