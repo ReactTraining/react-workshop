4940ccb3affcf99fada026087a35cdc2145ad28a
Brad Westfall <brad@builtwithweb.com>
2024-03-28 10:47:05 -0400

    small fix

--- react/_full-app/CenterContent.tsx
+++ react/_full-app/CenterContent.tsx
@@ -7,7 +7,7 @@ type CenterContentProps = {
 export function CenterContent({ children, className, maxWidth = 1200 }: CenterContentProps) {
   return (
     <div className={className}>
-      <div className={`ml-auto mr-auto pl-3 pr-3`} style={{ maxWidth: `${maxWidth}px` }}>
+      <div className="ml-auto mr-auto pl-3 pr-3" style={{ maxWidth: `${maxWidth}px` }}>
         {children}
       </div>
     </div>
