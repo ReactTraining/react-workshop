65792137c137dfe0bc2495a81013e589244a76a9
Brad Westfall <brad@builtwithweb.com>
2024-06-16 13:50:01 -0400

    split one lesson into two

--- react/advanced-hooks/04-transitions/lecture/index.tsx
+++ react/advanced-hooks/04-transitions/lecture/index.tsx
@@ -1,11 +1,6 @@
 import * as ReactDOM from 'react-dom/client'
 import { LessonBody, LessonCard } from '~/Lesson'
-
-// 1. Transitions for rendering performance
-import { App } from './rendering-performance'
-
-// 2. Form Actions
-// import { App } from './form-actions'
+import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <LessonBody>
