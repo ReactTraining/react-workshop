bc987602055fa6224edd817e8264636a81078b38
Brad Westfall <brad@builtwithweb.com>
2024-06-26 09:49:18 -0400

    fix starting point of lecture

--- react/advanced-hooks/03-optimistic-ui/lecture/App.tsx
+++ react/advanced-hooks/03-optimistic-ui/lecture/App.tsx
@@ -1,51 +1,14 @@
 import { useOptimistic, useState, useTransition } from 'react'
 import { type ResponseData, updateDatabase } from './helpers/mockServer'
 
-// export function App() {
-//   const [error, setError] = useState('')
-//   const [likes, setLikes] = useState(0)
-
-//   async function submit(e: React.FormEvent) {
-//     e.preventDefault()
-
-//     const data = (await updateDatabase(likes + 1).then((r) => r.json())) as ResponseData
-
-//     if (!data.error) {
-//       console.log(data.likes)
-//       setLikes(data.likes)
-//     } else {
-//       setLikes(data.likes)
-//       setError(data.error)
-//     }
-//   }
-
-//   return (
-//     <form onSubmit={submit} className="space-y-6">
-//       <div>
-//         <button type="submit" className="button text-xl">
-//           Like My Post: {likes}
-//         </button>
-//       </div>
-//       {error && <div className="text-red-800">{error}</div>}
-//       {/* <div>Optimistic Likes: {optimisticLikes}</div> */}
-//     </form>
-//   )
-// }
-
 export function App() {
   const [error, setError] = useState('')
   const [likes, setLikes] = useState(0)
 
-  const [optimisticLikes, setOptimisticLikes] = useOptimistic(
-    likes,
-    (currentLikes, newOptimisticLikes: number) => {
-      return newOptimisticLikes
-    }
-  )
+  async function submit(e: React.FormEvent) {
+    e.preventDefault()
 
-  async function action() {
-    setOptimisticLikes(optimisticLikes + 1)
-    const data = (await updateDatabase(optimisticLikes + 1).then((r) => r.json())) as ResponseData
+    const data = (await updateDatabase(likes + 1).then((r) => r.json())) as ResponseData
 
     if (!data.error) {
       console.log(data.likes)
@@ -57,10 +20,10 @@ export function App() {
   }
 
   return (
-    <form action={action} className="space-y-6">
+    <form onSubmit={submit} className="space-y-6">
       <div>
         <button type="submit" className="button text-xl">
-          Like My Post: {optimisticLikes}
+          Like My Post: {likes}
         </button>
       </div>
       {error && <div className="text-red-800">{error}</div>}
