d5b4ecca5432e8fe33f79d95c57f295aec16157b
Brad Westfall <brad@builtwithweb.com>
2024-08-14 14:27:54 -0400

    sm fix

--- react/advanced-component-design/02-form-abstractions/03-react-hook-form/index.tsx
+++ react/advanced-component-design/02-form-abstractions/03-react-hook-form/index.tsx
@@ -2,13 +2,13 @@ import * as ReactDOM from 'react-dom/client'
 import { LessonBody, LessonCard } from '~/Lesson'
 
 // Intro
-// import { LoginForm } from './start-here'
+import { LoginForm } from './start-here'
 
 // Basic Validation
 // import { LoginForm } from './basic-validation'
 
 // Zod Validation
-import { LoginForm } from './zod-validation'
+// import { LoginForm } from './zod-validation'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <LessonBody>
