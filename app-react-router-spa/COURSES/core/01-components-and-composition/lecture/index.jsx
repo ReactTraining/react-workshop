c8e01af7d586276de03c87a0483420a97d2f490a
Brad Westfall <brad@builtwithweb.com>
2025-11-14 16:32:19 -0500

    end

--- app-react-router-spa/COURSES/core/01-components-and-composition/lecture/index.jsx
+++ app-react-router-spa/COURSES/core/01-components-and-composition/lecture/index.jsx
@@ -1,12 +1,29 @@
 // import { jsx as _jsx, jsxs as _jsxs } from 'react/jsx-runtime'
 import React from 'react'
 import * as ReactDOM from 'react-dom/client'
-import { LessonBody, LessonCard } from '~/Lesson'
-import { Icon } from './examples/Icon'
-import { FaTrash } from 'react-icons/fa'
+// import { LessonBody, LessonCard } from '~/Lesson'
+// import { Icon } from './examples/Icon'
+// import { FaTrash } from 'react-icons/fa'
 
-function App() {
-  // Let's get started
+// Fn that returns a blueprint for UI (an object)
+// You have to invoke that fn correctly
+function Heading({ size, label, as: El, ...rest }) {
+  return (
+    <El {...rest} className={`heading heading-size-${size}`}>
+      {label}
+    </El>
+  )
+
+  // jsx(El) // string means make DOM <h2>
 }
 
-const el = document.getElementById('root')
+function App() {
+  return (
+    <div>
+      <Heading as="h4" label="My Heading Again" size={2} />
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
